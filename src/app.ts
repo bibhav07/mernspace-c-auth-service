5263c63568e4d37680f9eee41b19c61a1b1926fa
Bibhav Yadav <managementlucky07@gmail.com>
2023-12-04 18:32:21 +0530

    feat: express server

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import express from "express";
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send('welcome to auth service....')
-})
+app.get("/", (req, res) => {
+    res.send("welcome to auth service....");
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,18 @@
 import app from "./app";
 import { Config } from "./config";
 
-const startServer =  () => {
+const startServer = () => {
     const PORT = Config.PORT;
     try {
         app.listen(Config.PORT, () => {
             // eslint-disable-next-line no-console
             console.log(`Listening on port ${PORT}`);
-            
         });
     } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);
         process.exit(1);
-                
-        
     }
 };
-
 
 startServer();
