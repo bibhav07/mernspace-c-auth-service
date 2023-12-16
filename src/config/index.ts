aac0f8bd110ac9a26bedb2dd81dee18315a8d705
Bibhav Yadav <managementlucky07@gmail.com>
2023-12-16 20:08:42 +0530

    feat: setup database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,17 @@
 import { config } from "dotenv";
-config();
+import path from "path";
 
-const { PORT, NODE_ENV } = process.env;
+//setting dynamic path of env variables  TEST/DEV/PROD
+config({path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-export const Config = { PORT, NODE_ENV };
+const { PORT, NODE_ENV, DB_PORT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
+export const Config = { 
+    PORT, 
+    DB_PORT,
+    DB_NAME, 
+    DB_HOST, 
+    NODE_ENV, 
+    DB_USERNAME, 
+    DB_PASSWORD, 
+};
