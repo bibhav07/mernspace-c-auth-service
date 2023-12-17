e8b81c36d1de59ee83b8b58532139bdf0e64c8d8
Bibhav Yadav <managementlucky07@gmail.com>
2023-12-17 09:34:53 +0530

    fix: format

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,14 +3,13 @@ import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 
 export class AuthController {
-
-    constructor(private userService: UserService){
-        this.userService = userService
+    constructor(private userService: UserService) {
+        this.userService = userService;
     }
-    
+
     async register(req: RegisterUserRequest, res: Response) {
         const { firstName, lastName, email, password } = req.body;
-        await this.userService.create({firstName, lastName, email, password})
+        await this.userService.create({ firstName, lastName, email, password });
         res.status(201).json();
     }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,10 +2,14 @@ import { Repository } from "typeorm";
 import { User } from "../entity/User";
 import { userData } from "../types";
 
-export class UserService{
-    constructor(private userRepository: Repository<User>){}
-    async create({firstName, lastName, email, password}: userData){
-        await this.userRepository.save({ firstName, lastName, email, password });
-
+export class UserService {
+    constructor(private userRepository: Repository<User>) {}
+    async create({ firstName, lastName, email, password }: userData) {
+        await this.userRepository.save({
+            firstName,
+            lastName,
+            email,
+            password,
+        });
     }
 }
