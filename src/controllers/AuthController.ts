0732526100eed4ec100a160873048545b8216d0d
Bibhav Yadav <managementlucky07@gmail.com>
2023-12-26 22:24:02 +0530

    fix: format

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -84,7 +84,6 @@ export class AuthController {
         }
     }
     async login(req: RegisterUserRequest, res: Response, next: NextFunction) {
-        
         const result = validationResult(req);
         if (!result.isEmpty()) {
             return res.status(400).json({ errors: result.array() });
--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -5,7 +5,6 @@ import { AppDataSource } from "../../src/config/data-source";
 import { DataSource } from "typeorm";
 
 describe.skip("POST auth/login", () => {
-
     let connection: DataSource;
 
     beforeAll(async () => {
@@ -15,30 +14,31 @@ describe.skip("POST auth/login", () => {
     afterAll(async () => {
         await connection.destroy();
     });
-    
+
     describe("Given all fields", () => {
         it("should retun 200 after loign and attach cookie", async () => {
-    
             //---- arrage
             const userData = {
                 email: "by@gmail.com",
                 password: "secret",
-            }
-    
+            };
+
             //---- act
-           const response =  await request(app).post("/auth/login").send(userData);
-           
+            const response = await request(app)
+                .post("/auth/login")
+                .send(userData);
+
             //---- assert
             interface Headers {
-                ['set-cookie']: string[];
-            };
-    
+                ["set-cookie"]: string[];
+            }
+
             let accessToken = null;
             let refreshToken = null;
-    
+
             const cookies =
                 (response.headers as unknown as Headers)["set-cookie"] || [];
-    
+
             cookies.forEach((cookie) => {
                 if (cookie.startsWith("accessToken=")) {
                     accessToken = cookie.split(";")[0].split("=")[1];
@@ -47,18 +47,14 @@ describe.skip("POST auth/login", () => {
                     refreshToken = cookie.split(";")[0].split("=")[1];
                 }
             });
-    
+
             expect(response.statusCode).toBe(200);
-    
+
             expect(accessToken).not.toBe(null);
             expect(refreshToken).not.toBe(null);
-            
+
             expect(isJwt(accessToken)).toBeTruthy();
             expect(isJwt(refreshToken)).toBeTruthy();
-    
         });
-       
     });
-
-
 });
